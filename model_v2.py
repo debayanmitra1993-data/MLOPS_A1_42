235d750a4ac3e411693a8b36c894ff75a80a97c8
Debayan <debayanmitra1993@gmail.com>
2024-08-05 03:42:07 +0530

    DVC revert again

--- model_v2.py
+++ model_v2.py
@@ -30,7 +30,7 @@ def evaluate_model(model):
 # Function to train an SVM using GridSearchCV
 def train_svm():
     """Train an SVM using GridSearchCV on the dataset."""
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, )
     np.savetxt('./data_dvc/X_train.csv', X_train, delimiter=',', fmt='%d')
     np.savetxt('./data_dvc/y_train.csv', y_train, delimiter=',', fmt='%d')
     return
