f32af36c08bc85b74e938fca7c53e6fe1b956dce
Debayan <debayanmitra1993@gmail.com>
2024-08-07 01:49:41 +0530

    unit test correction

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,7 +1,7 @@
 import sys
 import unittest
 import os
-from model_v2 import train_decision_tree
+from model_v2 import train_svm
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
@@ -10,7 +10,7 @@ class TestModel(unittest.TestCase):
 
     def test_model_training(self):
         print("Running test_model_training")
-        model = train_decision_tree()
+        model = train_svm()
         self.assertIsNotNone(model)
 
 
